fb8ba8d12489948c5bf600518459710a42b73b01
zuxu4n <zuxu4n@gmail.com>
2026-05-24 18:10:29 -0400

    feat: Added GA4

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import { inject } from '@vercel/analytics';
 inject();
-
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
